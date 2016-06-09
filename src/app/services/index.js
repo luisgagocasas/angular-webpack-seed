439d5c846d4db8c95a06ba8c1c32aebfac3b0b48
Diego Silva <dsilva2401@gmail.com>
2016-06-09 00:31:38 -0500

    Libs service added

--- src/app/services/index.js
+++ src/app/services/index.js
@@ -1,5 +1,6 @@
 module.exports = function (app) {
 
+	app.factory('libs', require('./libs.factory.js'));
 	app.factory('network', require('./network.factory.js'));
 	app.factory('ui', require('./ui.factory.js'));
 
